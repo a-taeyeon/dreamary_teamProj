15dc49230ffe06bfc5ccd5a8b218eb7088e44dee
kimehhk <kimehhk@gmail.com>
2020-08-20 23:24:54 +0900

    first commit

--- dreamary/urls.py
+++ dreamary/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 import myapp.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', myapp.views.home, name='home'),
+    path('', include('myapp.urls')),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Designer
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    designer = Designer.objects.all()
+    return render(request, 'home.html', {'designers' : designer})
+
+def introduce(request):
+    return render(request, 'introduce.html')
