97c8ec3b79e2b17a755908948e420659bcc3251a
aaa <janu01ary@gmail.com>
2020-08-21 02:44:07 +0900

    new.html, update.html first commit

--- myapp/views.py
+++ myapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Designer
 
 # Create your views here.
@@ -8,3 +8,38 @@ def home(request):
 
 def introduce(request):
     return render(request, 'introduce.html')
+
+def new(request):
+    return render(request, 'new.html')
+
+def create(request):
+    if request.method == 'POST':
+        post = Designer()
+
+        if 'image' in request.FILES:
+            post.image = request.FILES['image']
+        post.name = request.POST['name']
+        post.address = request.POST['address']
+        post.description = request.POST['description']
+
+        post.save()
+
+        return redirect('introduce')  #여기 detail로 바꿔야함  ('detail', post.id)
+    else:
+        return render(request, 'new.html')
+
+def update(request, designer_id):
+    post = get_object_or_404(Designer, pk = designer_id)
+
+    if request.method == 'POST':
+        if 'image' in request.FILES:
+            post.image = request.FILES['image']
+        post.name = request.POST['name']    
+        post.address = request.POST['address']
+        post.description = request.POST['description']
+
+        post.save()
+
+        return redirect('introduce')#여기 detail로 바꿔야함 
+    else:
+        return render(request, 'update.html', {'designer' : post})    
